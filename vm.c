de0705fbe5e882c57a8691958c0557d278fdee90
Daniel Swann <dswann5@jhu.edu>
2015-10-18 22:46:57 -0400

    Added cow copyuvm with PTE_W flag unset

--- vm.c
+++ vm.c
@@ -338,6 +338,40 @@ bad:
   return 0;
 }
 
+// Given a parent process's page table, copy the pagedir and tables but not the page frames themselves.
+pde_t*
+cow_copyuvm(pde_t *pgdir, uint sz)
+{
+  pde_t *d;
+  pte_t *pte;
+  uint pa, i, flags;
+  char *mem;
+
+  if((d = setupkvm()) == 0)
+    return 0;
+  for(i = PGSIZE; i < sz; i += PGSIZE){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    flags &= ~PTE_W;
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)p2v(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, v2p(mem), flags) < 0)
+      goto bad;
+  }
+  return d;
+
+bad:
+  freevm(d);
+  return 0;
+}
+
+
+
 //PAGEBREAK!
 // Map user virtual address to kernel address.
 char*
