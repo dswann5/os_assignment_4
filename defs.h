e1633648c7acd2a747f0d0bf3061489eaeec218e
dlarej <liu.jerald@gmail.com>
2015-10-19 00:21:04 -0400

    Began modifications for part 3.

--- defs.h
+++ defs.h
@@ -170,10 +170,14 @@ void            freevm(pde_t*);
 void            inituvm(pde_t*, char*, uint);
 int             loaduvm(pde_t*, char*, struct inode*, uint, uint);
 pde_t*          copyuvm(pde_t*, uint);
+pde_t*          cow_copyuvm(pde_t*, uint);
 void            switchuvm(struct proc*);
 void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
+// Page fault handler
+int             handle_page_fault(uint);
+
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
